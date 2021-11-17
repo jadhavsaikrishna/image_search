3de760318ab8124dce02cbfe13de65edf0a33868
jadhav saikrishna <jadhavsaikrishna361@gmail.com>
2021-11-17 14:00:47 +0530

    img search

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import axios from  "axios";
+import { useState } from "react";
 
 function App() {
+
+
+  const [image, setImage] = useState("");
+  const clientId = "MMErkOferTQrF5FjOdDE7m99-cgKqKznSXqFs4XhzeI";
+  const [result, setResult] = useState([]);
+
+  const handleChange = (event) => {
+    setImage(event.target.value);
+    };
+    const handleSubmit = () => {
+      console.log(image);
+      
+    const url = "https://api.unsplash.com/search/photos?page=1&query=" +
+    image + "&client_id=" + clientId;
+    axios.get(url).then((response) => {
+    setResult(response.data.results);
+    });
+    };
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  <div className="app">
+  <div className="heading">
+   <h1>React Image Search Using Unsplash API.</h1>
+  </div>
+  <div className="input">
+   <input type="text" name="image" placeholder="Search for images" onChange={handleChange} type="text" name="image" placeholder="Search for images"  />
+   </div>
+   <button onClick={handleSubmit} type="submit">Search</button>
+  
+
+
+<div className="result">
+  {result.map((image) => (
+  <>
+   <div className="card">
+    <img src={image.urls.thumb} />
+    <p className="username"> Photo by {image.user.name}</p>
+    <p className="like">👍 {image.likes}</p>
+   </div>
+  </>
+   ))}
+</div>
+</div>
+);
+
+  
+  }
 
 export default App;
